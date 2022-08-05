8cea96c1136aa8bbbacb61c2540990e9f4bcdc7b
smsivanujan <smsivanujan@gmail.com>
2022-08-05 09:44:41 +0530

    noa template

---
+++ resources/views/layouts/components/styles.blade.php
@@ -0,0 +1,18 @@
+
+		<!-- TITLE -->
+		<title>gStudent</title>
+
+        <!-- FAVICON -->
+        <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/brand/logo-2.png')}}" />
+
+        <!-- BOOTSTRAP CSS -->
+        <link id="style" href="{{asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" />
+
+        <!-- STYLE CSS -->
+        <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" />
+        <link href="{{asset('assets/css/skin-modes.css')}}" rel="stylesheet" />
+
+        @yield('styles')
+
+        <!--- FONT-ICONS CSS -->
+        <link href="{{asset('assets/plugins/icons/icons.css')}}" rel="stylesheet" />
